29e3ab78e6740c9953c5193bf82452f26cfdaa3a
seksu <jarukit.suchat@gmail.com>
2017-06-29 15:30:46 +0700

    Add files via upload

--- testExpandIO/testExpandIO.ino
+++ testExpandIO/testExpandIO.ino
@@ -2,8 +2,10 @@
 #include "Adafruit_MCP23017.h"
 
 Adafruit_MCP23017 mcp[8];
-  
+const int UsDelay     = 100;
+
 void setup() {  
+  Serial.begin(115200);
   for(int i=0 ; i<8 ; i++){
     mcp[i].begin(i);
   }
@@ -14,8 +16,36 @@ void setup() {
   } 
 }
 
+void step(int row,int col,int steps){
+  int addr = (int)((row+1)*(col+1))/8;
+  int pin  = (int)(((row+1)*(col+1))%8)*2;
+  if(steps > 0){
+    mcp[addr].digitalWrite(pin,HIGH);
+  }
+  else(steps <= 0){
+    mcp[addr].digitalWrite(pin,LOW);
+  }
+  for(int i=0 ; i<abs(steps) ; i++){
+    mcp[addr].digitalWrite(pin+1,HIGH);
+    delay(UsDelay);
+    mcp[addr].digitalWrite(pin+1,LOW);
+    delay(UsDelay);
+  }
+}
+
 void loop() {
-  mcp[0].digitalRead(0,HIGH);
-  delay(500);
-  mcp[0].digitalRead(0,LOW);
+  if(Serial.available() > 0){
+    int row = Serial.parseInt();
+    int col = Serial.parseInt();
+    int steps = Serial.parseInt();
+
+    Serial.print("row : ");
+    Serial.println(row);
+    Serial.print("col : ");
+    Serial.println(col);
+    Serial.print("step : ");
+    Serial.println(steps);
+    Serial.println();
+    step(row,col,steps);
+  }
 }
