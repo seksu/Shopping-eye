8b69ed6f241ef625fbd98b36a2271267e61152ac
seksu <jarukit.suchat@gmail.com>
2017-06-29 10:31:11 +0700

    Add files via upload

---
+++ testExpandIO/testExpandIO.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include "Adafruit_MCP23017.h"
+
+Adafruit_MCP23017 mcp[8];
+  
+void setup() {  
+  for(int i=0 ; i<8 ; i++){
+    mcp[i].begin(i);
+  }
+  for(int i=0 ; i<8 ; i++){
+    for(int j=0 ; j<16 ; j++){
+      mcp[i].pinMode(j, OUTPUT);
+    }   
+  } 
+}
+
+void loop() {
+  mcp[0].digitalRead(0,HIGH);
+  delay(500);
+  mcp[0].digitalRead(0,LOW);
+}
